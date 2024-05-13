c7e73bcda731aa1e996d405587d83a5da2127226
hassan <emailer453@gmail.com>
2024-05-13 16:55:30 +0500

    Typewriter testing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,47 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
+import Typewriter from 'typewriter-effect';
+function Landing() {
+  return (
+    <h1 style={{color: 'red', fontWeight:"bold"}}> <Typewriter
+    onInit={(typewriter) => {
+      typewriter.typeString('Hello, World!')
+        .pauseFor(1000)
+        .deleteAll()
+        .typeString('Welcome to React Typewriter!')
+        .deleteAll()
+        .typeString('Get out hehe')
+        .start()
+    }}
 
-const Landing = () => {
-  const words = ["Hello", "World", "Landing", "Animation"];
-  const speed = 100; // typing speed in milliseconds
-  const delay = 2000; // delay between words in milliseconds
+    options={{
+      loop: false,
+      cursor: '_'
+    }}
+  /></h1>
+   
+  )
+}
 
-  const [text, setText] = useState('');
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [wordIndex, setWordIndex] = useState(0);
-
-  useEffect(() => {
-    let currentIndex = isDeleting ? text.length : 0;
-    let interval;
-
-    const type = () => {
-      const currentWord = words[wordIndex];
-
-      if (isDeleting) {
-        setText((prevText) => prevText.substring(0, currentIndex - 1));
-      } else {
-        setText((prevText) => currentWord.substring(0, currentIndex + 1));
-      }
-
-      if (!isDeleting && text === currentWord) {
-        setIsDeleting(true);
-        clearInterval(interval);
-        setTimeout(() => {
-          setIsDeleting(false);
-          setWordIndex((prevIndex) => (prevIndex + 1) % words.length);
-        }, delay);
-      } else if (isDeleting && text === '') {
-        clearInterval(interval);
-      }
-
-      currentIndex = isDeleting ? currentIndex - 1 : currentIndex + 1;
-    };
-
-    interval = setInterval(type, speed);
-
-    return () => clearInterval(interval);
-  }, [text, isDeleting, wordIndex]);
-
-  return <div>{text}</div>;
-};
-
-export default Landing;
+export default Landing
